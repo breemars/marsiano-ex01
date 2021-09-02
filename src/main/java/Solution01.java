3eadce6dd358ea2a3097f74903b63133f2330779
Bree <breemars@knights.ucf.edu>
2021-09-02 16:29:03 -0400

    Initial commit

---
+++ src/main/java/Solution01.java
@@ -0,0 +1,29 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Breanna Marsiano
+ */
+
+import java.util.Scanner;
+
+public class Solution01 {
+    /*
+    create scanner
+    ask user their name
+    input name as string
+    string concatenation
+    output the final string
+     */
+    public static void main(String[] args){
+
+        //IN - Asks the user for their name and stores it as a variable
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = input.nextLine();
+
+        //OUT - Prints out what the user's name is
+        String output = "Hello, " + name + ", nice to meet you!";
+        System.out.println(output);
+
+    }
+
+}

a8a30267e8e5fd0362e3df2f500e7035904f22fc
Bree <breemars@knights.ucf.edu>
2021-09-03 15:50:21 -0400

    fixed comment

--- src/main/java/Solution01.java
+++ src/main/java/Solution01.java
@@ -7,20 +7,21 @@ import java.util.Scanner;
 
 public class Solution01 {
     /*
-    create scanner
-    ask user their name
-    input name as string
-    create the string concatenation
-    output the final string
+    Output greeting
+    - create scanner
+    - ask user for their name
+    - input name as string
+    - create the string concatenation
+    - output the final string
      */
     public static void main(String[] args){
 
-        //IN - Asks the user for their name and stores it as a string
+        //IN - Asks the user for their name and store it as a string
         Scanner input = new Scanner(System.in);
         System.out.print("What is your name? ");
         String name = input.nextLine();
 
-        //OUT - Prints out what the user's name is
+        //OUT - Prints out a greeting with the user's name
         String output = "Hello, " + name + ", nice to meet you!";
         System.out.println(output);
 
